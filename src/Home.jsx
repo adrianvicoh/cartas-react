30e6717d489c5ea888239f673a6b4179e63139e4
KenjiPadilla <kenjipadilla4@gmail.com>
2024-07-03 20:01:25 +0200

    CarruselHeader iniciado

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import CarruselHeader from './components/CarruselHeader';
+import CarruselMarca from './components/CarruselMarca';
+import BaseController from './controllers/baseController';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Home() {
+  const [noticias, setNoticias] = useState([]);
+  const tableName = 'your_table_name'; 
+
+  useEffect(() => {
+    async function getAllNoticias() {
+      const noticiasController = new BaseController('m5hnoxg67k8a156', tableName);
+      const noticiasData = await noticiasController.getAll();
+      if (noticiasData.length) {
+        setNoticias(noticiasData);
+      } else {
+        console.log('No data found:', noticiasData);
+      }
+    }
+    getAllNoticias();
+  }, []);
 
   return (
     <>
-      <p>Home</p>
+    <Container>
+    <p>Home</p>
+      <Row>
+        <CarruselHeader noticias={noticias} />
+      </Row>
+      <Row>
+        <CarruselMarca noticias={noticias} />
+      </Row>
+    </Container>
     </>
-  )
+  );
 }
-
-export default Home
+export default Home;
