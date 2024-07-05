4f85e9c61e3bc9628b6cd3f292d3622d522c19b7
KenjiPadilla <kenjipadilla4@gmail.com>
2024-07-05 19:57:47 +0200

    carrusel de cartas sueltas en proceso

--- src/components/CarruselMarca.jsx
+++ src/components/CarruselMarca.jsx
@@ -1,21 +1,69 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import styled from 'styled-components';
 
-function CarruselMarca({ images }) {
+const ProductImage = styled.img`
+  width: 100%;
+  height: auto;
+  max-height: 50px; /* Ajusta la altura máxima de la imagen */
+`;
+
+const ProductCard = styled.div`
+  padding: 10px;
+  text-align: center;
+  max-width: 75px; /* Ajusta el ancho máximo del contenedor */
+  margin: auto; /* Centra el contenedor */
+  box-sizing: border-box;
+`;
+
+const ProductTitle = styled.h3`
+  font-size: 14px; /* Ajusta el tamaño del texto del título */
+`;
+
+const ProductDescription = styled.p`
+  font-size: 12px; /* Ajusta el tamaño del texto de la descripción */
+`;
+
+const CarruselMarca = ({ images }) => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2
+    }
+  };
+
   return (
-    <Carousel>
+    <Carousel 
+      responsive={responsive} 
+      infinite={true} 
+      autoPlay={true} 
+      autoPlaySpeed={3000}
+      swipeable={true}
+      draggable={true}
+      slidesToSlide={4} // Desliza 4 productos a la vez
+    >
       {images.map((image, index) => (
-        <Carousel.Item key={index}>
-          <img
-            className="d-block w-100"
-            src={image.url}
-            alt={`Slide ${index}`}
+        <ProductCard key={index}>
+          <ProductImage
+            src={image.mna1v9pmt69h5rd}
+            alt={`Product ${index}`}
           />
-          <Carousel.Caption>
-            <h3>{image.label}</h3>
-            <p>{image.Description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <ProductTitle>{image.Titulo}</ProductTitle>
+          <ProductDescription>{image.Description}</ProductDescription>
+        </ProductCard>
       ))}
     </Carousel>
   );
